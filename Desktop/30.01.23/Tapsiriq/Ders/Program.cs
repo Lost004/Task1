cfe7d096dc64cb2683d35abd9fe337a93d227828
Lost004 <nazimri@code.edu.com>
2023-01-29 12:07:27 -0800

    Tapsiriq 2

---
+++ Desktop/30.01.23/Tapsiriq/Ders/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ders
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Bir eded yazin!");
+			int n = Convert.ToInt32(Console.ReadLine());
+			int Cem = 0;
+			for (int i = 0; i < n; i++)
+			{
+				Cem += i;
+			}
+			Console.WriteLine("1 den'" + n + "reqemine qeder olan sayilarin toplami = " + Cem);
+			Console.ReadLine();
+		}
+	}
+}
